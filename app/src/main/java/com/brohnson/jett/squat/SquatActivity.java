f3c1f8c57ac1fc29eb0f051792cd92b18da38612
bretttjohnson1 <swimbrettjohnson@gmail.com>
2016-03-01 01:50:49 -0500

    implemented new types array

--- app/src/main/java/com/brohnson/jett/squat/SquatActivity.java
+++ app/src/main/java/com/brohnson/jett/squat/SquatActivity.java
@@ -17,55 +17,56 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class SquatActivity extends AppCompatActivity {
 
-    Squat squat;
+   Squat squat;
    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-       super.onCreate(savedInstanceState);
-       setContentView(R.layout.activity_squat);
-       squat = (Squat)getIntent().getParcelableExtra("Squat");
-       GraphView graph = (GraphView)findViewById(R.id.graph_squat);
-       int interval = 30;
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_squat);
+      squat = (Squat)getIntent().getParcelableExtra("Squat");
+      GraphView graph = (GraphView)findViewById(R.id.graph_squat);
+      int interval = 30;
 
-       DataPoint data[] = new DataPoint[(squat.end-squat.start)/interval+1];
-       if((squat.end-squat.start)%interval==0){
-           data = new DataPoint[(squat.end-squat.start)/interval];
-       }
-       Log.d("size", squat.end-squat.start + " " + squat.times.length + " " + data.length);
-       for(int a =squat.start;a<squat.end;a+=interval){
-           data[(a-squat.start)/interval] = new DataPoint(squat.times[a]-squat.times[squat.start],squat.angles[a]);
-       }
-
-
-       LineGraphSeries<DataPoint> linegraph = new LineGraphSeries<DataPoint>(data);
-       linegraph.setColor(ContextCompat.getColor(this,R.color.Blue600));
-       linegraph.setTitle("Angle");
-       graph.addSeries(linegraph);
-       DataPoint[] zero = new DataPoint[2];
-       zero[0] = new DataPoint(0,Squat.REQUIRED_DEPTH);
-       zero[1] = new DataPoint(squat.times[squat.end]-squat.times[squat.start]+500,Squat.REQUIRED_DEPTH);
-       LineGraphSeries<DataPoint> zeroline = new LineGraphSeries<DataPoint>(zero);
-       zeroline.setTitle("Required Depth ("+Squat.REQUIRED_DEPTH+(char) 0x00B0+")");
-       zeroline.setColor(Color.RED);
-       graph.addSeries(zeroline);
-       graph.getViewport().setYAxisBoundsManual(true);
-       graph.getViewport().setXAxisBoundsManual(true);
-       graph.getViewport().setMaxX(squat.times[squat.end] - squat.times[squat.start] + 500);
-       graph.getViewport().setMinY(((int) (-10 + squat.depth) / 10) * 10 - 10);
-       graph.getViewport().setMaxY(90);
-       graph.getLegendRenderer().setVisible(true);
-       graph.getLegendRenderer().setBackgroundColor(Color.TRANSPARENT);
-       graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
-       graph.getViewport().setScalable(true);
-       graph.getViewport().setScrollable(true);
+      DataPoint data[] = new DataPoint[(squat.end-squat.start)/interval+1];
+      if((squat.end-squat.start)%interval==0){
+         data = new DataPoint[(squat.end-squat.start)/interval];
+      }
+      Log.d("size", squat.end-squat.start + " " + squat.times.length + " " + data.length);
+      for(int a =squat.start;a<squat.end;a+=interval){
+         data[(a-squat.start)/interval] = new DataPoint(squat.times[a]-squat.times[squat.start],squat.angles[a]);
+      }
 
 
-       ListView listview = (ListView)findViewById(R.id.individual_stats_listview);
-       int values[] = new int[]{squat.depth, squat.averageupspeed,squat.averagedownspeed,squat.maxupspeed,squat.maxdropspeed,(int)(squat.endtimeunder-squat.starttimeunder)};
-       String names[] = new String[]{"Depth","Average Upward Angular Speed","Average Downward Angular Speed","Max Upward Angular Speed","Max Downward Angular Speed","Time At Bottom"};
-       listview.setAdapter(new SquatStatsArrayAdapter(this,R.id.individual_stats_listview,names,values));
+      LineGraphSeries<DataPoint> linegraph = new LineGraphSeries<DataPoint>(data);
+      linegraph.setColor(ContextCompat.getColor(this,R.color.Blue600));
+      linegraph.setTitle("Angle");
+      graph.addSeries(linegraph);
+      DataPoint[] zero = new DataPoint[2];
+      zero[0] = new DataPoint(0,Squat.REQUIRED_DEPTH);
+      zero[1] = new DataPoint(squat.times[squat.end]-squat.times[squat.start]+500,Squat.REQUIRED_DEPTH);
+      LineGraphSeries<DataPoint> zeroline = new LineGraphSeries<DataPoint>(zero);
+      zeroline.setTitle("Required Depth ("+Squat.REQUIRED_DEPTH+(char) 0x00B0+")");
+      zeroline.setColor(Color.RED);
+      graph.addSeries(zeroline);
+      graph.getViewport().setYAxisBoundsManual(true);
+      graph.getViewport().setXAxisBoundsManual(true);
+      graph.getViewport().setMaxX(squat.times[squat.end] - squat.times[squat.start] + 500);
+      graph.getViewport().setMinY(((int) (-10 + squat.depth) / 10) * 10 - 10);
+      graph.getViewport().setMaxY(90);
+      graph.getLegendRenderer().setVisible(true);
+      graph.getLegendRenderer().setBackgroundColor(Color.TRANSPARENT);
+      graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
+      graph.getViewport().setScalable(true);
+      graph.getViewport().setScrollable(true);
+
+
+      ListView listview = (ListView)findViewById(R.id.individual_stats_listview);
+      int types[] = new int[]{1,0,0,0,0,2};
+      int values[] = new int[]{squat.depth, squat.averageupspeed,squat.averagedownspeed,squat.maxupspeed,squat.maxdropspeed,(int)(squat.endtimeunder-squat.starttimeunder)};
+      String names[] = new String[]{"Depth","Average Upward Angular Speed","Average Downward Angular Speed","Max Upward Angular Speed","Max Downward Angular Speed","Time At Bottom"};
+      listview.setAdapter(new SquatStatsArrayAdapter(this,R.id.individual_stats_listview,names,values,types));
 
 
 
 
-    }
+   }
 }

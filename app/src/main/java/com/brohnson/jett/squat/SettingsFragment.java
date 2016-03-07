ebd3211a054a759d7ae4d66d061cee887b830fac
bretttjohnson1 <swimbrettjohnson@gmail.com>
2016-03-06 21:26:52 -0500

    Settings Added sliders and value checking

--- app/src/main/java/com/brohnson/jett/squat/SettingsFragment.java
+++ app/src/main/java/com/brohnson/jett/squat/SettingsFragment.java
@@ -5,14 +5,14 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.GridView;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 
@@ -43,6 +43,105 @@ public class SettingsFragment extends Fragment implements View.OnClickListener{
         ed.setText(reqdepth + "");
         EditText ed2 = (EditText)rootView.findViewById(R.id.countdown_edit_text);
         ed2.setText(timer+"");
+        ed2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                try{
+                    if(Integer.parseInt(s.toString())<0 || Integer.parseInt(s.toString())>20) {
+                        EditText ed2 = (EditText) rootView.findViewById(R.id.countdown_edit_text);
+
+                        ed2.setText(StartFragment.COUNTDOWN + "");
+                    }else {
+                        SeekBar seek = (SeekBar) rootView.findViewById(R.id.seekBar2);
+                        seek.setProgress(Integer.parseInt(s.toString()));
+                    }
+
+                }catch (Exception e){
+                    EditText ed2 = (EditText)rootView.findViewById(R.id.countdown_edit_text);
+
+                    ed2.setText(StartFragment.COUNTDOWN + "");
+                }
+            }
+        });
+        ed.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                try{
+                    if(Integer.parseInt(s.toString())<0 || Integer.parseInt(s.toString())>40) {
+
+                        EditText ed = (EditText) rootView.findViewById(R.id.required_depth_edit_text);
+                        ed.setText(Squat.REQUIRED_DEPTH + "");
+                    }else {
+
+                        SeekBar seek = (SeekBar) rootView.findViewById(R.id.seekBar);
+                        seek.setProgress(Integer.parseInt(s.toString()));
+                    }
+
+                }catch (Exception e){
+                    EditText ed = (EditText)rootView.findViewById(R.id.required_depth_edit_text);
+
+                    ed.setText(Squat.REQUIRED_DEPTH + "");
+                }
+            }
+        });
+        SeekBar seek = (SeekBar)rootView.findViewById(R.id.seekBar);
+        seek.setProgress(reqdepth);
+        seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                EditText ed = (EditText)rootView.findViewById(R.id.required_depth_edit_text);
+                ed.setText((progress-20)+"");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        SeekBar seek2 = (SeekBar)rootView.findViewById(R.id.seekBar2);
+        seek2.setProgress(timer);
+        seek2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                EditText ed2 = (EditText)rootView.findViewById(R.id.countdown_edit_text);
+                ed2.setText(progress+"");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
         return rootView;
 
     }
@@ -62,14 +161,25 @@ public class SettingsFragment extends Fragment implements View.OnClickListener{
             }catch (Exception e){
 
             }
-            editor.putInt(mainactivity.getString(R.string.REQUIRED_DEPTH),reqdepth);
-            editor.putInt(mainactivity.getString(R.string.COUNTDOWN),timer);
-
-            Squat.REQUIRED_DEPTH = reqdepth;
-            StartFragment.COUNTDOWN=timer;
+            boolean isnotgood = true;
+            if(!(reqdepth>20 || reqdepth<-40)) {
+                editor.putInt(mainactivity.getString(R.string.REQUIRED_DEPTH), reqdepth);
+                Squat.REQUIRED_DEPTH = reqdepth;
+                isnotgood=false;
+            } else
+                ed.setText(Squat.REQUIRED_DEPTH+"");
+            if(!(timer<0 || timer>60)) {
+                editor.putInt(mainactivity.getString(R.string.COUNTDOWN), timer);
+                StartFragment.COUNTDOWN = timer;
+                isnotgood=false;
+            } else
+                ed2.setText(StartFragment.COUNTDOWN + "");
             editor.commit();
-            Toast.makeText(mainactivity, "Settings Saved", Toast.LENGTH_SHORT).show();
+            if(!isnotgood)
+                Toast.makeText(mainactivity, "Settings Saved", Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(mainactivity, "Setting Out of Bounds", Toast.LENGTH_SHORT).show();
+
         }
     }
-
 }
